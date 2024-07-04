e8635aff575b68627b3ca5719b69b4c241981318
anirbanchoudhury <anirban.choudhury@vantagecircle.com>
2024-07-04 21:08:38 +0530

    checkBalanced Binary Tree

--- src/CrackTheCodingInterviewQuestions/TreesAndGraphs/CheckBalanced.java
+++ src/CrackTheCodingInterviewQuestions/TreesAndGraphs/CheckBalanced.java
@@ -1,6 +1,10 @@
 package CrackTheCodingInterviewQuestions.TreesAndGraphs;
 
+import Utilities.BinaryTree;
+import Utilities.BinaryTreePrinter;
 import Utilities.TNode;
+
+import static Helper.Helper.db;
 
 public class CheckBalanced {
 
@@ -10,7 +14,46 @@ public class CheckBalanced {
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
-    public static void main(String[] args) {
+    public boolean checkBalanced(TNode<Integer> root) {
 
+        if(root == null) return true;
+
+        int lst = height(root.left);
+        int rst = height(root.right);
+
+        if(Math.abs(lst - rst) > 1) return false;
+        return checkBalanced(root.left) && checkBalanced(root.right);
+    }
+
+    private int checkHeight(TNode<Integer> node) {
+        if(node == null) return 0;
+
+        int lst = checkHeight(node.left);
+        if(lst == -1) return -1;
+
+        int rst = checkHeight(node.right);
+        if(rst == -1) return -1;
+
+        if(Math.abs(lst - rst) > 1) return -1;
+
+        return Math.max(lst, rst) + 1;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        TNode<Integer> root = new TNode<>(20);
+
+        bt.addNode(root, new TNode<>(12));
+        bt.addNode(root, new TNode<>(11));
+        bt.addNode(root, new TNode<>(1));
+        bt.addNode(root, new TNode<>(234));
+        bt.addNode(root, new TNode<>(874));
+        bt.addNode(root, new TNode<>(98));
+        bt.addNode(root, new TNode<>(3982));
+
+        BinaryTreePrinter<Integer> btp = new BinaryTreePrinter<>();
+        btp.printTree(root);
+        db("\nHeight: ", height(root));
+        db("\nBF: ", height(root.left) - height(root.right));
     }
 }
