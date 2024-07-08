94d84f4f3898bf2556171d6f584e34e2f3bbbafa
anirbanchoudhury <anirban.choudhury@vantagecircle.com>
2024-07-08 10:36:07 +0530

    BST added

---
+++ src/Utilities/BinarySearchTrees.java
@@ -0,0 +1,41 @@
+package Utilities;
+
+public class BinarySearchTrees<T extends Comparable<T>> {
+    TNode<T> root;
+
+    public BinarySearchTrees(TNode<T> root) {
+        this.root = root;
+    }
+
+    public void addNode(TNode<T> r, TNode<T> node) {
+
+        if(root == null) {
+            root = node;
+            return;
+        }
+
+        if(node.data.compareTo(r.data) <= 0) {
+            if(r.left == null) {
+                r.left = node;
+            } else addNode(r.left, node);
+        } else {
+            if(r.right == null) {
+                r.right = node;
+            } else addNode(r.right, node);
+        }
+    }
+
+    public static void main(String[] args) {
+        TNode<Integer> root = new TNode<>(7);
+        BinarySearchTrees bst = new BinarySearchTrees(root);
+        bst.addNode(root, new TNode<>(9));
+        bst.addNode(root, new TNode<>(10));
+        bst.addNode(root, new TNode<>(6));
+        bst.addNode(root, new TNode<>(5));
+        bst.addNode(root, new TNode<>(7));
+
+        BinaryTreePrinter<Integer> btp = new BinaryTreePrinter<>();
+
+        btp.printTree(root);
+    }
+}
